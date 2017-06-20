1674afce6f31bc158b135c9c7713f5b98e30b056
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-06-20 17:16:10 +0800

    init

--- src/pages/speech/speech.ts
+++ src/pages/speech/speech.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { SpeechRecognition, SpeechRecognitionListeningOptionsAndroid, SpeechRecognitionListeningOptionsIOS } from '@ionic-native/speech-recognition';
 
 @IonicPage({
   name: 'SpeechPage'
@@ -11,44 +11,72 @@ import { SpeechRecognition } from '@ionic-native/speech-recognition';
 })
 export class SpeechPage {
 
-  text: string;
+  speechList: Array<string> = [];
+  androidOptions: SpeechRecognitionListeningOptionsAndroid;
+  iosOptions: SpeechRecognitionListeningOptionsIOS;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public speechRecognition: SpeechRecognition) {
-    // Check feature available
-    this.speechRecognition.isRecognitionAvailable()
-      .then((available: boolean) => console.log(available))
-
-    // Start the recognition process
-    this.speechRecognition.startListening({})
-      .subscribe(
-      (matches: Array<string>) => console.log(matches),
-      (onerror) => console.log('error:', onerror)
-      );
-
-    // Stop the recognition process (iOS only)
-    this.speechRecognition.stopListening()
-
-    // Get the list of supported languages
-    this.speechRecognition.getSupportedLanguages()
-      .then(
-      (languages: Array<string>) => console.log(languages),
-      (error) => console.log(error)
-      );
-
-    // Check permission
-    this.speechRecognition.hasPermission()
-      .then((hasPermission: boolean) => console.log(hasPermission));
-
-    // Request permissions
-    this.speechRecognition.requestPermission()
-      .then(
-      () => console.log('Granted'),
-      () => console.log('Denied')
-      );
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform, public speechRecognition: SpeechRecognition) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SpeechPage');
+  }
+
+  async isSpeechSupported(): Promise<boolean> {
+    let isAvailable = await this.speechRecognition.isRecognitionAvailable();
+    console.log(isAvailable);
+    return isAvailable;
+  }
+
+  async getPermission(): Promise<void> {
+    try {
+      let permission = await this.speechRecognition.requestPermission();
+      console.log(permission);
+      return permission;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+
+  async hasPermission(): Promise<boolean> {
+    try {
+      let permission = await this.speechRecognition.hasPermission();
+      console.log(permission);
+      return permission;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+
+  async getSupportedLanguages(): Promise<Array<string>> {
+    try {
+      let languages = await this.speechRecognition.getSupportedLanguages();
+      console.log(languages);
+      return languages;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+
+  listenForSpeech(): void {
+
+    this.androidOptions = {
+      prompt: 'Speak into your phone!'
+    }
+
+    this.iosOptions = {
+      language: 'en-US'
+    }
+
+    if (this.platform.is('android')) {
+      this.speechRecognition.startListening(this.androidOptions).subscribe(data => this.speechList = data, error => console.log(error));
+    }
+    else if (this.platform.is('ios')) {
+      this.speechRecognition.startListening(this.iosOptions).subscribe(data => this.speechList = data, error => console.log(error));
+    }
   }
 
 }
