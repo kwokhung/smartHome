54faed2430cfadc51ffd61d3171561839454e528
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-06-20 16:28:20 +0800

    init

--- src/pages/speech/speech.ts
+++ src/pages/speech/speech.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
 @IonicPage({
   name: 'SpeechPage'
@@ -13,20 +13,42 @@ export class SpeechPage {
 
   text: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public tts: TextToSpeech) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public speechRecognition: SpeechRecognition) {
+    // Check feature available
+    this.speechRecognition.isRecognitionAvailable()
+      .then((available: boolean) => console.log(available))
+
+    // Start the recognition process
+    this.speechRecognition.startListening({})
+      .subscribe(
+      (matches: Array<string>) => console.log(matches),
+      (onerror) => console.log('error:', onerror)
+      );
+
+    // Stop the recognition process (iOS only)
+    this.speechRecognition.stopListening()
+
+    // Get the list of supported languages
+    this.speechRecognition.getSupportedLanguages()
+      .then(
+      (languages: Array<string>) => console.log(languages),
+      (error) => console.log(error)
+      );
+
+    // Check permission
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => console.log(hasPermission));
+
+    // Request permissions
+    this.speechRecognition.requestPermission()
+      .then(
+      () => console.log('Granted'),
+      () => console.log('Denied')
+      );
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SpeechPage');
-  }
-
-  async sayText(): Promise<any> {
-    try {
-      await this.tts.speak(this.text);
-    }
-    catch (e) {
-      console.log(e);
-    }
   }
 
 }
